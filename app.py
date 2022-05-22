7efc03b1f4736e21e38689043af373c361b78e02
sorapong <33795437+serversuck@users.noreply.github.com>
2022-05-22 10:54:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # Importing essential libraries
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,jsonify
 import pickle
+from pythainlp import word_tokenize as ptk
 from sklearn.feature_extraction.text import CountVectorizer , TfidfTransformer 
 count_vec = CountVectorizer(tokenizer=ptk)
 tfidf = TfidfTransformer()
 
 
-# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
 filename = 'spammodel.pkl'
 model = pickle.load(open(filename, 'rb'))
 count_vec = pickle.load(open('count_vec.pkl','rb'))
@@ -17,6 +17,7 @@ def predicttt(x):
   tfidf.fit(xvec) 
   Xtfidf = tfidf.fit_transform(xvec) 
   r = model.predict(Xtfidf)
+  
   return r
 
 @app.route('/')
@@ -28,8 +29,20 @@ def predict():
     if request.method == 'POST':
     	message = request.form['message']
     	data = [message]
-    	my_prediction = predicttt(data)
-    	return render_template('result.html', prediction=my_prediction)
+    	my_prediction = predicttt(data)[0]
+    	print(my_prediction)
+    	return render_template('result.html', prediction=str(my_prediction))
+
+@app.route('/api',methods=['GET'])
+def api():
+    if request.method == 'GET':
+    	
+    	message =request.args.get('msg')
+    	data = [message]
+    	my_prediction = predicttt(data)[0]
+    	print(my_prediction)
+    	result = {'result':my_prediction}
+    	return jsonify(result)
 
 if __name__ == '__main__':
 	app.run(debug=True)
