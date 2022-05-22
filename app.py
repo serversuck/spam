80bd46cd0268b0ea1d94e9785d3e24315736b592
sorapong <33795437+serversuck@users.noreply.github.com>
2022-05-22 10:30:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 # Importing essential libraries
 from flask import Flask, render_template, request
 import pickle
+from sklearn.feature_extraction.text import CountVectorizer , TfidfTransformer 
+count_vec = CountVectorizer(tokenizer=ptk)
+tfidf = TfidfTransformer()
+
 
 # Load the Multinomial Naive Bayes model and CountVectorizer object from disk
 filename = 'spammodel.pkl'
 model = pickle.load(open(filename, 'rb'))
 count_vec = pickle.load(open('count_vec.pkl','rb'))
 app = Flask(__name__)
+
+def predicttt(x):
+  xvec = count_vec.transform(x) 
+  tfidf.fit(xvec) 
+  Xtfidf = tfidf.fit_transform(xvec) 
+  r = model.predict(Xtfidf)
+  return r
 
 @app.route('/')
 def home():
@@ -17,8 +28,7 @@ def predict():
     if request.method == 'POST':
     	message = request.form['message']
     	data = [message]
-    	vect = cv.transform(data).toarray()
-    	my_prediction = classifier.predict(vect)
+    	my_prediction = predicttt(data)
     	return render_template('result.html', prediction=my_prediction)
 
 if __name__ == '__main__':
